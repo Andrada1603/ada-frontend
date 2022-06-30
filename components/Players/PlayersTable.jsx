cd8912d761fbf62090c645b4d6cdce3e394a3a3d
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2022-06-30 12:09:52 +0300

    uses infinitequery

--- components/Players/PlayersTable.jsx
+++ components/Players/PlayersTable.jsx
@@ -1,20 +1,18 @@
 import { useRouter } from 'next/router';
 import { deletePlayer } from '../../api/player';
-import { useQuery } from '../../hooks';
+import { useInfiniteQuery } from '../../hooks';
 import Button from '../Button';
 import DeleteRow from '../DeleteRow';
 
 const PlayersTable = () => {
   const router = useRouter();
-  const { data } = useQuery(`/players`);
-
-  const downloadData = () => alert('downloading');
+  const { data } = useInfiniteQuery(`/players`);
 
   const renderPlayer = ({ _id, first_name, last_name, birthday, email, category }, index) => {
     const handleClick = (id) => router.push(`/admin/players/${id}`);
 
     return (
-      <tr key={`player-${first_name}-${last_name}`} onClick={() => handleClick(_id)}>
+      <tr key={`player-${_id}-${first_name}-${last_name}`} onClick={() => handleClick(_id)}>
         <td>{index + 1}</td>
         <td>
           <strong>
@@ -34,12 +32,15 @@ const PlayersTable = () => {
   return (
     <div>
       <div className="mb-6 flex justify-between w-full">
-        <Button className="button full primary"><i className="fa fa-plus mr-4" />Adaugă jucător</Button>
-        <Button className="button full secondary"><i className="fa fa-download mr-4" />Descarcă datele</Button>
+        <Button className="button full primary">
+          <i className="fa fa-plus mr-4" />
+          Adaugă jucător
+        </Button>
+        <Button className="button full secondary">
+          <i className="fa fa-download mr-4" />
+          Descarcă datele
+        </Button>
       </div>
-      {/* <h4 className="text-lg mb-4 cursor-pointer" onClick={downloadData}>
-        Descarcă datele
-      </h4> */}
       <table>
         <thead>
           <th>#</th>
@@ -49,8 +50,11 @@ const PlayersTable = () => {
           <th>Email</th>
           <th></th>
         </thead>
-        <tbody>{data?.pages.map(renderPlayer)}</tbody>
+        <tbody>{data?.pages[0].map(renderPlayer)}</tbody>
       </table>
+      <Button className="button full secondary my-4">
+        Incarca mai mult
+      </Button>
       {/* <h4 className="my-4"> Au fost gasiti {data?.pageParams.count} jucători în baza de date.</h4> */}
     </div>
   );
