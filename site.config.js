75133f59d21e763c6c2f25620613a320e8e1e8a5
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2022-06-30 16:47:29 +0300

    Adds more FA icons

--- site.config.js
+++ site.config.js
@@ -6,6 +6,7 @@ module.exports = {
   image: `${process.env.APP_BASE_URL}/images/logo.png`,
   stylesheets: [
     'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
   ],
