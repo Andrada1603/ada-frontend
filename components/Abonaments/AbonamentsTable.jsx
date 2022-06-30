225d3395082940146bcdce523b0c9e2b051e3ab6
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2022-06-30 12:40:52 +0300

    Adds active to abonaments table

--- components/Abonaments/AbonamentsTable.jsx
+++ components/Abonaments/AbonamentsTable.jsx
@@ -1,22 +1,28 @@
 import { deleteAbonament } from '../../api/abonament';
 import { useQuery } from '../../hooks';
-import { format } from 'date-fns';
+import { add, format } from 'date-fns';
 import DeleteRow from '../DeleteRow';
 
 const AbonamentsTable = () => {
   const { data } = useQuery(`/abonaments`);
 
-  const renderArea = ({_id, abonament_type, starting_date, period, price }, index) => {
+  const renderAbonament = ({ _id, abonament_type, starting_date, period, price }, index) => {
+    const expirationDate = add(new Date(starting_date), {
+      days: period,
+    });
+    const isActive = expirationDate > Date.now();
+
     return (
-      <tr key={`area-${price}`}>
+      <tr key={`area-${_id}-${price}`}>
         <td>{index + 1}</td>
         <td>
-          <strong>
-            {abonament_type}
-          </strong>
+          <strong>{abonament_type}</strong>
         </td>
         <td>{format(new Date(starting_date), 'dd-MM-yyyy')}</td>
-        <td>{period} zile</td>
+        <td>
+          {isActive ? <span className="table-pill bg-green-500">ACTIV</span>
+          : <span className="table-pill bg-accent">EXPIRAT</span>}
+          </td>
         <td>{price} LEI</td>
         <td className="text-center py-0">
           <DeleteRow id={_id} action={deleteAbonament} />
@@ -27,17 +33,17 @@ const AbonamentsTable = () => {
 
   return (
     <div>
-      <h4 className='mb-4'> Au fost gasite {data?.pageParams.count} abonamente în baza de date</h4>
+      <h4 className="mb-4"> Au fost gasite {data?.pageParams.count} abonamente în baza de date</h4>
       <table>
         <thead>
           <th>#</th>
           <th>Tip abonament</th>
           <th>Data început</th>
-          <th>Perioada</th>
+          <th>Status</th>
           <th>Preț</th>
           <th></th>
         </thead>
-        <tbody>{data?.pages.map(renderArea)}</tbody>
+        <tbody>{data?.pages.map(renderAbonament)}</tbody>
       </table>
     </div>
   );
