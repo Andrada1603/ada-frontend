6262e6cce1ca9992625c68e0b122b5a2637b7db6
ANDRADA POPA <POPAANDRADA19@stud.ase.ro>
2022-07-15 02:28:03 +0300

    Player Info backup option

--- components/Players/PlayerInfo.jsx
+++ components/Players/PlayerInfo.jsx
@@ -5,28 +5,47 @@ const PlayerInfo = ({ id, first_name, last_name, birthday, email, category, ...p
   return (
     <>
       <div className="player-info">
-        <h3 className="text-3xl text-primary">{fullName({ first_name, last_name })}</h3>
-        <div class= "text-xs text-white py-1 px-2 rounded uppercase text-center cursor-default font-semibold bg-primary"> Activ </div>
+        <div className="flex flex-wrap gap-2 items-center">
+        <h3 className="text-3xl text-primary">{fullName({ first_name, last_name })} </h3>
+        <div className="hidden sm:flex">
+        <div class="text-xs text-white py-1 px-2 rounded uppercase text-center cursor-default font-semibold bg-primary" > Activ </div>
+          </div>
+          </div>
+
         {/* YOUR CODE HERE */}
-        <div className="flex flex-col w-full px-4 lg:px-12 py-8 my-8 bg-white rounded-lg max-w-xl">
-          <img class='avatar h-72 w-full md:w-80 rounded-md' src='/images/smile.jpg' align= 'left'
+        <div className="flex flex-col w-full px-4 lg:px-7 py-8 my-10 bg-white rounded-lg max-w-xl">
+          <div className="grid md:grid-cols-2 gap-4">
+          <img class='avatar h-72 w-full md:w-80 rounded-md' src='/images/smile.jpg'
             alt='PlayerPhoto'></img>
-            <h3 align='right'> <strong> Informații </strong> </h3>
-          <div class="flex flex-col">
-          <span class='text-primary' align='right' vertical-align='top'> <strong> Nume: </strong> </span>
+            <div class="flex flex-col gap-2">
+              <h3 align='right'> <strong> Informații </strong> </h3>
+
+              <div class="flex flex-col">
+            <span class='text-primary' align='right' > <strong> Nume: </strong> </span>
             <span align='right'> {last_name} </span>
             </div>
-          <span class='text-primary' align='right'> <strong> Prenume: </strong> </span>
+
+              <div class="flex flex-col">
+              <span class='text-primary' align='right'> <strong> Prenume: </strong> </span>
           <span align='right'> {first_name} </span>
-          <span class='text-primary' align='right'> <strong> Data nașterii: </strong> </span>
+              </div>
+
+              <div class="flex flex-col">
+              <span class='text-primary' align='right'> <strong> Data nașterii: </strong> </span>
           <span align='right'>{format(new Date(birthday), 'dd-MM-yyyy')} </span>
-          <span class='text-primary' align='right'> <strong> Contact: </strong> </span>
+              </div>
+
+              <div class="flex flex-col">
+              <span class='text-primary' align='right'> <strong> Contact: </strong> </span>
           <span align='right'> {email} </span>
-          <span class='text-primary' align='right'> <strong> Categorie sportivă: </strong> </span>
-          <span align='right'> {category} </span>
-          <div class='flex items-center justify-between w-60 px-3 py-2 rounded text-white bg-primary font-semibold'>
-            <p class='py-1'> Rating </p>
+              </div>
+
+              <div class="flex flex-col">
+              <span class='text-primary' align='right'> <strong> Categorie sportivă: </strong> </span>
+                <span align='right'> {category} </span>
+              </div>
             </div>
+          </div>
         </div>
       </div>
       
