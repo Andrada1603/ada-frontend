4bb7d0938e659d376d9a793d53e76c49d184d5e8
ANDRADA POPA <POPAANDRADA19@stud.ase.ro>
2022-07-15 13:53:37 +0300

    adds validation

--- models/signup.js
+++ models/signup.js
@@ -1,12 +1,12 @@
 import * as Yup from 'yup';
 
 export const validationSchema = Yup.object().shape({
-  last_name: Yup.string().required(),
-  first_name: Yup.string().required(),
-  birthday: Yup.string().required(),
-  email: Yup.string().email().required(),
-  category: Yup.string().required(),
-  password: Yup.string().min(8).required(),
+  last_name: Yup.string().required('Numele este obligatoriu'),
+  first_name: Yup.string().required('Prenumele este obligatoriu'),
+  birthday: Yup.string().required('Data nasterii este obligatorie'),
+  email: Yup.string().email().required('Emailul este obligatoriu si trebuie sa raspecte formatul aaa@bbb.ccc'),
+  category: Yup.string().required('Daca nu detineti un titlu sportiv completati cu FC'),
+  password: Yup.string().min(8).required('Parola trebuie sa aibă minim 8 caractere'),
 });
 
 export const initialValues = {
